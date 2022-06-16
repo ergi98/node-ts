758a6a397f35cfa8cfbb26943398023dcc497147
ergi98 <edervishaj98@gmail.com>
2022-06-16 16:52:12 +0200

    Absolute paths

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,35 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "prettier", "import"],
   rules: {
-    indent: ["error", 2],
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
+    "prettier/prettier": "error",
+    "import/extensions": "off",
+    "no-console": "off",
+    "import/no-unresolved": "error",
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+        project: "./tsconfig.json",
+      },
+    },
   },
 };
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  useTabs: false,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  tabWidth: 2,
+  printWidth: 80,
+  endOfLine: "auto",
+  trailingComma: "es5",
+  arrowParens: "avoid", // Include parentheses around a sole arrow function parameter.
+};
