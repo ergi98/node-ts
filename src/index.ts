9daa7bc8aae89da3e78ba2dd2d007cbaa9167776
ergi98 <edervishaj98@gmail.com>
2022-06-16 16:20:45 +0200

    Dotenv for loading the correct env file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-function test() {
-  const message: string = "Test";
-  console.log({ message });
-}
-test();
+import dotenv from "dotenv";
+import path from "path";
+
+import express, { Application, Request, Response, NextFunction } from "express";
+
+dotenv.config({
+  path: path.resolve(__dirname, "..", `env.${process.env.NODE_ENV}`),
+});
+
+const app: Application = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello");
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Listening on port ${process.env.PORT}`)
+);
